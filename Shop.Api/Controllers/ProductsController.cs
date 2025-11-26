1c82d54434e3b97de21ca56089944e71f90b2b19
OODRUG <gurievdania@gmail.com>
2025-11-26 19:11:44 +0100

    Add the ProductsController for the Api requests handling implemented. Add Program.cs is implemented for the start of the service.

---
+++ Shop.Api/Controllers/ProductsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Application.DTOs;
+using Shop.Application.Services;
+
+namespace Shop.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetAll()
+        {
+            var products = await _productService.GetAllAsync();
+            return Ok(products);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductDto>> GetById(int id)
+        {
+            var product = await _productService.GetByIdAsync(id);
+            if (product == null)
+                return NotFound($"Product with Id{id} not found.");
+            return Ok(product);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateProductDto createProductDto)
+        {
+            var newProductId = await _productService.CreateAsync(createProductDto);
+            return CreatedAtAction(nameof(GetById), new { id = newProductId }, null);
+        }
+        [HttpPut("{id}/stock")]
+        public async Task<IActionResult> UpdateStock(int id, [FromBody] UpdateStockDto dto)
+        {
+            try
+            {
+                await _productService.UpdateStockAsync(id, dto.NewQuantity);
+                return NoContent();
+            }
+            catch (KeyNotFoundException ex)
+            {
+                throw NotFound(ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
--- Shop.Api/Program.cs
+++ Shop.Api/Program.cs
@@ -1,41 +1,56 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+using Shop.Application.DTOs;
+using Shop.Application.Services;
+using Shop.Application.Validators;
+using Shop.Domain.Interfaces;
+using Shop.Infrastructure.Data;
+using Shop.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? "Data Source=shop.db";
+builder.Services.AddDbContext<ShopDbContext>(options =>
+    options.UseSqlite(connectionString));
+//register layers(DI)
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductService, ProductService>();
+
+//register validators
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddScoped<IValidator<CreateProductDto>, CreateProductValidator>();
+//swagger 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Shop API",
+        Version = "v1",
+        Description = "An e-shop API for managing products."
+    });
+});
 
 var app = builder.Build();
+
+// ensure db created or connected
+using (var scope = app.Services.CreateScope()) { 
+    var dbContext = scope.ServiceProvider.GetRequiredService<ShopDbContext>();
+    dbContext.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
