ec0cb1b0444afe85955744a01cddfe0e44072cc7
OODRUG <gurievdania@gmail.com>
2025-11-26 15:55:42 +0100

    Add the Validators folder and CreateProductValidator.cs for the validation.

---
+++ Shop.Application/Validators/CreateProductValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Shop.Application.DTOs;
+
+namespace Shop.Application.Validators
+{
+    public class CreateProductValidator : AbstractValidator<CreateProductDto>
+    {
+        public CreateProductValidator() 
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Product name is required.").MaximumLength(100);
+            //Handing the absolte URL validation **
+            RuleFor(x => x.ImgUrl).NotEmpty().WithMessage("Image Url is required.").Must(uri=>Uri.TryCreate(uri, UriKind.Absolute, out _)).WithMessage("Image Url must be a valid URL.");
+        }
+    }
+}
