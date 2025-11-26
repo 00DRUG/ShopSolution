2e1922485df97495f625f4091acf5e1851b5e87a
OODRUG <gurievdania@gmail.com>
2025-11-26 15:46:44 +0100

    Add the Dtos to the Application layer for the secure handling and control. ProductDtos.cs

---
+++ Shop.Application/DTOs/ProductDtos.cs
@@ -0,0 +1,18 @@
+﻿namespace Shop.Application.DTOs
+{
+    //data struct for the user requesting 
+    public record ProductDto(
+        int Id,
+        string Name,
+        string ImgUrl,
+        decimal Price,
+        string? Description,
+        int StockQuantity);
+
+    //requirement of creating the product from the task
+    public record CreateProductDto(
+        string Name,
+        string ImgUrl);
+    public record UpdateStockDto(
+        int NewQuantity);
+}
