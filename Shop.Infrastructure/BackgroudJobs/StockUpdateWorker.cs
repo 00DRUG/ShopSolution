477c9356fc6df2c722407551ea9ed2d8db9824f4
OODRUG <gurievdania@gmail.com>
2025-11-30 18:28:42 +0100

    Implementation of StockUpdateWorker service.

--- Shop.Infrastructure/BackgroudJobs/StockUpdateWorker.cs
+++ Shop.Infrastructure/BackgroudJobs/StockUpdateWorker.cs
@@ -1,12 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Shop.Application.Services;
 
 namespace Shop.Infrastructure.BackgroudJobs
 {
-    internal class StockUpdateWorker
+    public class StockUpdateWorker : BackgroundService
     {
+        private readonly IStockQueue _stockQueue;
+        private readonly IServiceScopeFactory _serviceScopeFactory; // Factory temp to create scopes
+        private readonly ILogger<StockUpdateWorker> _logger;
+        public StockUpdateWorker(IStockQueue stockQueue,
+            IServiceScopeFactory serviceScopeFactory,
+            ILogger<StockUpdateWorker> logger)
+        {
+            _stockQueue = stockQueue;
+            _serviceScopeFactory = serviceScopeFactory;
+            _logger = logger;
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Stock Update Worker running.");
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var stockUpdateMessage = await _stockQueue.DequeueAsync(stoppingToken);
+                using (var scope = _serviceScopeFactory.CreateScope())
+                {
+                    var productService = scope.ServiceProvider.GetRequiredService<IProductService>();
+                    try
+                    {
+                        await productService.UpdateStockAsync(stockUpdateMessage.ProductId, stockUpdateMessage.NewQuantity);
+                        _logger.LogInformation($"Updated stock for ProductId: {stockUpdateMessage.ProductId} to NewQuantity: {stockUpdateMessage.NewQuantity}");
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError(ex, $"Error updating stock for ProductId: {stockUpdateMessage.ProductId}");
+                    }
+                }
+            }
+        }
     }
 }
