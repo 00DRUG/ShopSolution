6b5c2e728b45c4b12b0d1d27ae7c3f4aca178731
OODRUG <gurievdania@gmail.com>
2025-11-30 18:25:18 +0100

    Create StockQueue  as a service *PIPE*

---
+++ Shop.Infrastructure/BackgroudJobs/StockQueue.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Channels;
+using System.Threading.Tasks;
+using System.Threading;
+using Shop.Application.DTOs;
+
+namespace Shop.Infrastructure.BackgroudJobs
+{
+    public interface IStockQueue
+    {
+        ValueTask QueueBackgroundWorkItemAsync(StockUpdateMessage workItem);
+        ValueTask<StockUpdateMessage> DequeueAsync(CancellationToken cancellationToken);
+    }
+    public class StockQueue : IStockQueue
+    {
+        private readonly Channel<StockUpdateMessage> _channel;
+        public StockQueue()
+        {
+            var options = new BoundedChannelOptions(100)
+            {
+                FullMode = BoundedChannelFullMode.Wait
+            };
+            _channel = Channel.CreateBounded<StockUpdateMessage>(options);
+        }
+        public async ValueTask QueueBackgroundWorkItemAsync(StockUpdateMessage workItem)
+        {
+            if (workItem == null)
+            {
+                throw new ArgumentNullException(nameof(workItem));
+            }
+            await _channel.Writer.WriteAsync(workItem);
+        }
+        public async ValueTask<StockUpdateMessage> DequeueAsync(CancellationToken cancellationToken)
+        {
+            return await _channel.Reader.ReadAsync(cancellationToken);
+        }
+    }
+}
