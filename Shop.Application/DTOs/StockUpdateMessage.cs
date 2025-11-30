15be3713b9ed4b9042bbfc79646c263246bee682
OODRUG <gurievdania@gmail.com>
2025-11-30 18:07:25 +0100

    Create StockUpdateMessage in DTOs for the Async Queue

---
+++ Shop.Application/DTOs/StockUpdateMessage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.DTOs
+{
+    public record StockUpdateMessage(int ProductId, int NewQuantity);
+}
